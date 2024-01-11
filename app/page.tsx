4ad9befef86dd1fe410b632a425088af2f4b2edb
Dario Turin <dturin1997@gmail.com>
2024-01-11 00:47:30 -0600

    Home Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
+import Image from "next/image";
+import Type from "@components/Type";
+
 export default function Home() {
-  return <div>This is my home page</div>;
+  return (
+    <section>
+      <div className="flex flex-row items-center justify-between px-2 lg:px-[200px]">
+        <div className="">
+          <div className="home-header">
+            <div>
+              <h1 className="heading">Hi There! 👋🏻</h1>
+
+              <h1 className="heading-name">
+                I'M <strong className="main-name">DARIO TURIN</strong>
+              </h1>
+              <div style={{ textAlign: "left" }}>
+                <Type />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-7">
+          <Image
+            src="/DarioTurin.jpeg"
+            alt="Landscape picture"
+            width={250}
+            height={250}
+            className="home-image"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Resume() {
-  return <div>resume page</div>;
+  return <></>;
 }
