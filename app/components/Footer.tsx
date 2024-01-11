63eb3aa2a54e405f308b22ec55a63db05cebed87
Dario Turin <dturin1997@gmail.com>
2024-01-10 22:23:10 -0600

    Adding changes to Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,59 @@
-import React from "react";
+"use client";
+
+import { Typography } from "@material-tailwind/react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t border-blue-gray-50">
+      <div className="container mx-auto">
+        <div className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 py-6 text-center md:justify-between">
+          <Typography color="blue-gray" className="font-normal">
+            &copy; 2023 Material Tailwind
+          </Typography>
+          <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+            <li>
+              <Typography
+                as="a"
+                href="#"
+                color="blue-gray"
+                className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+              >
+                About Us
+              </Typography>
+            </li>
+            <li>
+              <Typography
+                as="a"
+                href="#"
+                color="blue-gray"
+                className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+              >
+                License
+              </Typography>
+            </li>
+            <li>
+              <Typography
+                as="a"
+                href="#"
+                color="blue-gray"
+                className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+              >
+                Contribute
+              </Typography>
+            </li>
+            <li>
+              <Typography
+                as="a"
+                href="#"
+                color="blue-gray"
+                className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+              >
+                Contact Us
+              </Typography>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
